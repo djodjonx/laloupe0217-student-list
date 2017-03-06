5db1b12b80d3610ad12078e6ef0927776b96f443
Jonathan Moutier <djo.moutier@gmail.com>
2017-03-06 11:46:12 +0100

    Edit studentsList.js

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,15 @@
 // Write yout Javascript code in this files
+console.log("salut");
+var request = new XMLHttpRequest();  //création d'une variable avec la classe XMLHttpRequest
+request.open('GET', 'students.json', true); //récpération des données dans le fichier student.json
+request.onreadystatechange = function () { //fonction qui se lancera à chaque changement de ready state
+  if (request.readyState == 4) {  //méthode qui s'effectuera a chaque changement dans notre requète
+     if(request.status == 200) {//si la requète 200 (tout c'est bien passé)
+var students = JSON.parse(request.responseText);  //créer une variable students qui récupérera les les donnée du fihier JSON en format text
+     console.log(students);
+   } else {
+      dump("Erreur pendant le chargement de la page.\n");
+    }
+  }
+};
+request.send()
